621fa73e849fea32b9bb23f26dda3ceede042d11
takasing <takaty2011@gmail.com>
2020-05-06 16:14:44 +0900

    separate styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-  title = 'angular8-example';
+  title = 'This is angular8-example';
 }
