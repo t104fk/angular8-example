739bbad3c11e5007868457fd9966474dc337e9b1
takasing <takaty2011@gmail.com>
2020-08-11 18:48:23 +0900

    refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactComponent } from './react/react.component';
+import { GuideComponent } from './guide/guide.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'react', component: ReactComponent },
+  { path: 'guide', component: GuideComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppToolbar } from './template/toolbar.component';
+import { AppToolbar } from './templates/toolbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactComponent } from './react/react.component';
+import { GuideComponent } from './guide/guide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppToolbar,
+    ReactComponent,
+    GuideComponent,
   ],
   imports: [
     BrowserModule,
